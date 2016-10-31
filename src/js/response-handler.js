b6cd217be8349b99dbfcc45843082276fc681475
danb-cws <sugarlumpltd@gmail.com>
2016-10-31 10:18:22 +0000

    reworked error handling

--- src/js/response-handler.js
+++ src/js/response-handler.js
@@ -8,17 +8,22 @@ for now, just take the first match that's not in the boringMatches array
 
 import giphyRequest from './giphy-request';
 
-let labelsLength;
+// let labelsLength;
+let labels;
 const boringMatches = ['room', 'property', 'image', 'black', 'white', 'color', 'interior design', 'real estate', 'person', 'man made object'];
 
 export default function responseHandler(data) {
-  const labels = data.responses[0].labelAnnotations;
+  if (data.responses === undefined) {
+    giphyRequest(null); // will invoke error msg
+    return;
+  }
+  labels = data.responses[0].labelAnnotations;
   if (labels === undefined) {
     giphyRequest(null); // will invoke error msg
     return;
   }
-  labelsLength = labels.length;
-  for (let i = 0; i < labelsLength; i += 1) {
+  // labelsLength = labels.length;
+  for (let i = 0; i < labels.length; i += 1) {
     if (boringMatches.indexOf(labels[i].description) < 0) {
       giphyRequest(labels[i].description); // we found a match thats not in the boring list, so submit and bail here
       return;
