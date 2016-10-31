a27d8a179bcd26b45324171c642eadd036275fb3
danb-cws <sugarlumpltd@gmail.com>
2016-10-31 10:13:00 +0000

    add check for hiding cam toggle button when on file input flow auto clicking gifyfy button on file select add capture camera attribute to input

--- src/js/file-input-fallback.js
+++ src/js/file-input-fallback.js
@@ -8,15 +8,26 @@ import * as config from './giphyvision-config';
 import * as mediaHandler from './media-handler';
 
 let fileInput;
+let fileInputCopyNode;
+let inputFilesRef;
+let hasCamToggle;
+
 function copyToImageElem() {
-  const inputFilesRef = window.URL.createObjectURL(fileInput.files[0]);
+  hasCamToggle = document.querySelector(`#${config.uiCameraToggleId}`);
+  if (hasCamToggle) { // hide cam toggle if 1)has 2 cams and 2)user has refused permission
+    hasCamToggle.setAttribute('style', 'display: none');
+  }
+  inputFilesRef = window.URL.createObjectURL(fileInput.files[0]);
+  config.uiImagePreview.src = inputFilesRef;
+  mediaHandler.mediaOnload();
   config.uiStatusElem.innerHTML = '';
   config.uiRepeatBtn.setAttribute('style', 'display: none');
   config.uiCaptureCtrls.setAttribute('style', 'display: inline-block');
   config.uiCaptureBtn.removeAttribute('disabled');
-  config.uiImagePreview.src = inputFilesRef;
-  mediaHandler.mediaOnload();
   config.uiOnboardingElem.classList.add('hidden');
+  setTimeout(() => {
+    config.uiCaptureBtn.click();
+  }, 300);
 }
 
 function nullFileInput() { // to fix issue of choosing same file again not firing onchange
@@ -32,13 +43,14 @@ export function pseudoClickFileInput(event) {
 
 export function invokeFileInput() {
   if (!fileInput) {
-    const fileInputCopyNode = document.createElement('p');
+    fileInputCopyNode = document.createElement('p');
     fileInputCopyNode.innerHTML = `${config.uiFallbackFileInputCopy}`;
     config.uiOnboardingElem.appendChild(fileInputCopyNode);
     fileInput = document.createElement('input');
     fileInput.id = 'fileInput';
     fileInput.type = 'file';
     fileInput.accept = 'image/*';
+    fileInput.capture = 'camera';
     fileInput.style.visibility = 'hidden';
     fileInput.addEventListener('click', nullFileInput, false);
     fileInput.addEventListener('change', copyToImageElem, false);
