86d5ac66f2ef2985da42bb83dba871a9b3ee512c
danb-cws <sugarlumpltd@gmail.com>
2016-11-28 21:16:46 +0000

    minor edit

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -20,7 +20,7 @@ module.exports = {
     publicPath: 'https://192.168.1.16:8080/', // absolute path req here for assets in css to work with sourcemaps on. Must be actual numeric ip to access over lan.
     filename: 'js/[name].js',
   },
-  // profile: true, // show times for build of each chunk etc, to debug slow builds
+  // profile: true, // show times for build of each chunk etc to debug slow builds
   debug: true,
   devtool: 'source-map',
   devServer: {
