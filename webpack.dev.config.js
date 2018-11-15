d73de3ecba9d19c5041005bb1d93991eb89af13a
danb-cws <sugarlumpltd@gmail.com>
2018-11-15 23:55:09 +0000

    edit config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -16,8 +16,8 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: 'http://localhost:8080/', // toggle to this to test non https
-    publicPath: 'https://192.168.1.16:8080/', // absolute path req here for assets in css to work with sourcemaps on. Must be actual numeric ip to access over lan.
+    publicPath: 'https://localhost:8080/', // toggle to this to test non https
+    // publicPath: 'https://192.168.1.16:8080/', // absolute path req here for assets in css to work with sourcemaps on. Must be actual numeric ip to access over lan.
     filename: 'js/[name].js',
   },
   // profile: true, // show times for build of each chunk etc to debug slow builds
